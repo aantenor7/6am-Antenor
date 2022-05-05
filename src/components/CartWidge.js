9559255c0c901ae9b15e24bf97eff154e9e8cc00
aantenor7 <aantenor7@gmail.com>
2022-05-04 21:29:41 -0300

    CartWidge

---
+++ src/components/CartWidge.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BsCart} from 'react-icons/bs';
+
+
+
+const CartWidge = () => {
+    return (
+    <div>
+        
+        <BsCart size={40}/>
+     </div>
+    
+    
+    
+    );
+};
+
+export default CartWidge;   
